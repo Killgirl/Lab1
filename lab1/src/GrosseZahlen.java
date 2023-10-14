8e35df56188d1d45d961df5ed43d4ab7edc831c6
Andreea <142813007+Killgirl@users.noreply.github.com>
2023-10-14 03:05:26 +0300

    3a

--- lab1/src/GrosseZahlen.java
+++ lab1/src/GrosseZahlen.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 public class GrosseZahlen {
     public int[] array;
 
@@ -6,7 +7,14 @@ public class GrosseZahlen {
         this.array = arr;
     }
     public int[] SummeArr(GrosseZahlen other){
-
+        int[] arrfinal={};
+        for(int i=0; i< array.length; i++){
+            arrfinal = Arrays.copyOf(arrfinal, arrfinal.length + 1);
+            arrfinal[arrfinal.length - 1] = array[i]+other.array[i];
+        }
+        return arrfinal;
     }
+
+
 
 }
--- lab1/src/Main.java
+++ lab1/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -6,5 +8,8 @@ public class Main {
         ArrZahlen arr = new ArrZahlen(array);
 
         System.out.println(arr.maximZahl());
+        GrosseZahlen arr3 = new GrosseZahlen(array);
+        GrosseZahlen arr4 = new GrosseZahlen(array);
+        System.out.println(Arrays.toString(arr3.SummeArr(arr4)));
     }
 }
