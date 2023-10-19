705d78f735a224aca9fd9c3c6c709cd2ba1f7405
Andreea <142813007+Killgirl@users.noreply.github.com>
2023-10-19 10:09:42 +0300

    test3

--- lab1/src/TestGrosseZahlen.java
+++ lab1/src/TestGrosseZahlen.java
@@ -6,11 +6,13 @@ public class TestGrosseZahlen {
 
         int[] array = {1,3,6,2};
         int[] array2 = {1,1,4,1};
+        int[] array3 = {};
+        GrosseZahlen arr5 = new GrosseZahlen(array3);
         GrosseZahlen arr3 = new GrosseZahlen(array);
         GrosseZahlen arr4 = new GrosseZahlen(array2);
         assert Arrays.equals(arr3.SummeArr(arr4), new int[]{2, 5, 0, 3});
         try {
-            arr3.SummeArr(arr4);
+            arr5.SummeArr(arr4);
             assert false;
         }
         catch(Exception e)
@@ -23,11 +25,13 @@ public class TestGrosseZahlen {
 
         int[] array = {1,3,6,2};
         int[] array2 = {1,1,4,1};
+        int[] array3 = {};
+        GrosseZahlen arr5 = new GrosseZahlen(array3);
         GrosseZahlen arr3 = new GrosseZahlen(array);
         GrosseZahlen arr4 = new GrosseZahlen(array2);
         assert Arrays.equals(arr3.DifferenzArr(arr4), new int[]{2,2,1});
         try {
-            arr3.DifferenzArr(arr4);
+            arr5.DifferenzArr(arr4);
             assert false;
         }
         catch(Exception e)
@@ -40,11 +44,13 @@ public class TestGrosseZahlen {
 
         int[] array = {1,3,6,2};
         int[] array2 = {1,1,4,1};
+        int[] array3 = {};
+        GrosseZahlen arr5 = new GrosseZahlen(array3);
         GrosseZahlen arr3 = new GrosseZahlen(array);
         GrosseZahlen arr4 = new GrosseZahlen(array2);
         assert Arrays.equals(arr3.MultiplikationArr(9), new int[]{1, 2, 2, 5, 8});
         try {
-            arr3.MultiplikationArr(9);
+            arr5.MultiplikationArr(9);
             assert false;
         }
         catch(Exception e)
@@ -56,11 +62,13 @@ public class TestGrosseZahlen {
     public static void testDivision() throws Exception{
         int[] array = {1,3,6,2};
         int[] array2 = {1,1,4,1};
+        int[] array3 = {};
+        GrosseZahlen arr5 = new GrosseZahlen(array3);
         GrosseZahlen arr3 = new GrosseZahlen(array);
         GrosseZahlen arr4 = new GrosseZahlen(array2);
         assert Arrays.equals(arr3.DivisionArr(2), new int[]{6, 8, 1});
         try {
-            arr3.DivisionArr(2);
+            arr5.DivisionArr(2);
             assert false;
         }
         catch(Exception e)
