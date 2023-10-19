5b3b83c7b50f9a0f6f14dec55574f271d7efbd94
Andreea <142813007+Killgirl@users.noreply.github.com>
2023-10-19 10:07:48 +0300

    test2

--- lab1/src/TestArrZahlen.java
+++ lab1/src/TestArrZahlen.java
@@ -4,9 +4,11 @@ public class TestArrZahlen {
 
         int[] array = {1,3,6,2};
         ArrZahlen arr = new ArrZahlen(array);
+        int[] array1 ={};
+        ArrZahlen arr2 = new ArrZahlen(array1);
         assert(arr.maximZahl()==6);
         try {
-            arr.maximZahl();
+            arr2.maximZahl();
             assert false;
         }
         catch(Exception e)
@@ -18,9 +20,11 @@ public class TestArrZahlen {
     public static void testMinZahl() throws Exception{
         int[] array = {1,3,6,2};
         ArrZahlen arr = new ArrZahlen(array);
+        int[] array1 ={};
+        ArrZahlen arr2 = new ArrZahlen(array1);
         assert (arr.minZahl()==1);
         try {
-            arr.minZahl();
+            arr2.minZahl();
             assert false;
         }
         catch(Exception e)
@@ -33,9 +37,11 @@ public class TestArrZahlen {
     public static void testMinSumme() throws Exception{
         int[] array = {1,3,6,2};
         ArrZahlen arr = new ArrZahlen(array);
+        int[] array1 ={};
+        ArrZahlen arr2 = new ArrZahlen(array1);
         assert (arr.minSumme()==6);
         try {
-            arr.minSumme();
+            arr2.minSumme();
             assert false;
         }
         catch(Exception e)
@@ -48,9 +54,11 @@ public class TestArrZahlen {
     public static void testMaxSumme() throws Exception{
         int[] array = {1,3,6,2};
         ArrZahlen arr = new ArrZahlen(array);
+        int[] array1 ={};
+        ArrZahlen arr2 = new ArrZahlen(array1);
         assert (arr.maxSumme()==11);
         try {
-            arr.maxSumme();
+            arr2.maxSumme();
             assert false;
         }
         catch(Exception e)
