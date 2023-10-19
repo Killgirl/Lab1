d774d57651fd87593d481a1532a4af2b71287d0c
Andreea <142813007+Killgirl@users.noreply.github.com>
2023-10-19 10:12:32 +0300

    test4

--- lab1/src/TestShop.java
+++ lab1/src/TestShop.java
@@ -4,10 +4,12 @@ public class TestShop {
 
         int[] arrShop = {10, 60, 25, 80};
         int[] arrShop2 = {15, 30, 20, 40, 50};
+        int[] arrShop3 = {};
+        ElektronikShop shop2= new ElektronikShop(arrShop3, arrShop2);
         ElektronikShop shop1 =new ElektronikShop(arrShop, arrShop2);
         assert (shop1.billigsteTastatur()==10);
         try {
-            shop1.billigsteTastatur();
+            shop2.billigsteTastatur();
             assert false;
         }
         catch(Exception e)
@@ -20,10 +22,12 @@ public class TestShop {
 
         int[] arrShop = {10, 60, 25, 80};
         int[] arrShop2 = {15, 30, 20, 40, 50};
+        int[] arrShop3 = {};
+        ElektronikShop shop2= new ElektronikShop(arrShop3, arrShop2);
         ElektronikShop shop1 =new ElektronikShop(arrShop, arrShop2);
         assert (shop1.teuersteGegenstand()==80);
         try {
-            shop1.teuersteGegenstand();
+            shop2.teuersteGegenstand();
             assert false;
         }
         catch(Exception e)
@@ -35,8 +39,10 @@ public class TestShop {
     public static void testUsbKauf() throws Exception{
         int[] arrShop = {10, 60, 25, 80};
         int[] arrShop2 = {15, 30, 20, 40, 50};
+        int[] arrShop3 = {};
+        ElektronikShop shop2= new ElektronikShop(arrShop3, arrShop2);
         ElektronikShop shop1 =new ElektronikShop(arrShop, arrShop2);
-        assert (shop1.usbKauf(43)==40);
+        assert (shop2.usbKauf(43)==40);
         try {
             shop1.usbKauf(43);
             assert false;
@@ -50,8 +56,10 @@ public class TestShop {
     public static void testKauf() throws Exception{
         int[] arrShop = {10, 60, 25, 80};
         int[] arrShop2 = {15, 30, 20, 40, 50};
+        int[] arrShop3 = {};
+        ElektronikShop shop2= new ElektronikShop(arrShop3, arrShop2);
         ElektronikShop shop1 =new ElektronikShop(arrShop, arrShop2);
-        assert (shop1.kauf(43)==40);
+        assert (shop2.kauf(43)==40);
         try {
             shop1.kauf(43);
             assert false;
