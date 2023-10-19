abae1c5919e4c4a1b6ac8005210d32e838361aa9
Andreea <142813007+Killgirl@users.noreply.github.com>
2023-10-19 10:01:41 +0300

    testShop

--- lab1/src/Main.java
+++ lab1/src/Main.java
@@ -4,10 +4,6 @@ import java.util.Arrays;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) throws Exception {
-        int[] array = {1,3,6,2};
-        int[] array2 = {1,1,4,1};
-        int[] arrShop = {10, 60, 25, 80};
-        int[] arrShop2 = {15, 30, 20, 40, 50};
 
         TestNoten.testNichtAusreichend();
         TestNoten.testGerundeteNoten();
@@ -21,17 +17,9 @@ public class Main {
         TestGrosseZahlen.testDifferenz();
         TestGrosseZahlen.testMultiplikation();
         TestGrosseZahlen.testDivision();
-
-        ElektronikShop shop1 =new ElektronikShop(arrShop, arrShop2);
-        assert (shop1.billigsteTastatur()==10);
-        //assert (shop1.billigsteTastatur()==25);
-        assert (shop1.teuersteGegenstand()==80);
-        //assert (shop1.teuersteGegenstand()==60);
-        assert (shop1.usbKauf(43)==40);
-        //assert (shop1.usbKauf(43)==50);
-        assert (shop1.kauf(43)==40);
-        //assert (shop1.kauf(43)==-1);
-        System.out.println("Teste Pb4");
-        System.out.println("Teste");
+        TestShop.testBilligsteTastatur();
+        TestShop.testTeuersteGegenstand();
+        TestShop.testUsbKauf();
+        TestShop.testKauf();
     }
 }
--- lab1/src/TestShop.java
+++ lab1/src/TestShop.java
@@ -1,3 +1,66 @@
 public class TestShop {
-    
+
+    public static void testBilligsteTastatur() throws Exception{
+
+        int[] arrShop = {10, 60, 25, 80};
+        int[] arrShop2 = {15, 30, 20, 40, 50};
+        ElektronikShop shop1 =new ElektronikShop(arrShop, arrShop2);
+        assert (shop1.billigsteTastatur()==10);
+        try {
+            shop1.billigsteTastatur();
+            assert false;
+        }
+        catch(Exception e)
+        {
+            assert true;
+        }
+    }
+
+    public static void testTeuersteGegenstand() throws Exception{
+
+        int[] arrShop = {10, 60, 25, 80};
+        int[] arrShop2 = {15, 30, 20, 40, 50};
+        ElektronikShop shop1 =new ElektronikShop(arrShop, arrShop2);
+        assert (shop1.teuersteGegenstand()==80);
+        try {
+            shop1.teuersteGegenstand();
+            assert false;
+        }
+        catch(Exception e)
+        {
+            assert true;
+        }
+    }
+
+    public static void testUsbKauf() throws Exception{
+        int[] arrShop = {10, 60, 25, 80};
+        int[] arrShop2 = {15, 30, 20, 40, 50};
+        ElektronikShop shop1 =new ElektronikShop(arrShop, arrShop2);
+        assert (shop1.usbKauf(43)==40);
+        try {
+            shop1.usbKauf(43);
+            assert false;
+        }
+        catch(Exception e)
+        {
+            assert true;
+        }
+    }
+
+    public static void testKauf() throws Exception{
+        int[] arrShop = {10, 60, 25, 80};
+        int[] arrShop2 = {15, 30, 20, 40, 50};
+        ElektronikShop shop1 =new ElektronikShop(arrShop, arrShop2);
+        assert (shop1.kauf(43)==40);
+        try {
+            shop1.kauf(43);
+            assert false;
+        }
+        catch(Exception e)
+        {
+            assert true;
+        }
+
+
+    }
 }
