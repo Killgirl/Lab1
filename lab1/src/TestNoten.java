f27b8c5e25bf2584fb0640d5759f3ed88e762eb8
Andreea <142813007+Killgirl@users.noreply.github.com>
2023-10-19 10:05:59 +0300

    test1

--- lab1/src/TestNoten.java
+++ lab1/src/TestNoten.java
@@ -6,9 +6,12 @@ public class TestNoten {
 
         int[] arrNoten = {39, 44, 57, 76, 80, 99, 17};
         Noten noten = new Noten(arrNoten);
+        int[] arr2Noten = {};
+        Noten noten1 = new Noten(arr2Noten);
+
         assert Arrays.equals(noten.nichtAusreichend(), new int[]{20});
         try {
-            noten.nichtAusreichend();
+            noten1.nichtAusreichend();
             assert false;
         }
         catch(Exception e)
@@ -21,9 +24,12 @@ public class TestNoten {
     public static void testGerundeteNoten() throws Exception{
         int[] arrNoten = {39, 44, 57, 76, 80, 99, 17};
         Noten noten = new Noten(arrNoten);
+        int[] arr2Noten = {};
+        Noten noten1 = new Noten(arr2Noten);
+
         assert Arrays.equals(noten.gerundeteNoten(), new int[]{17, 100, 85, 80, 60, 45, 40});
         try {
-            noten.gerundeteNoten();
+            noten1.gerundeteNoten();
             assert false;
         }
         catch(Exception e)
@@ -35,9 +41,11 @@ public class TestNoten {
     public static void testGrossteGerundete() throws Exception{
         int[] arrNoten = {39, 44, 57, 76, 80, 99, 17};
         Noten noten = new Noten(arrNoten);
+        int[] arr2Noten = {};
+        Noten noten1 = new Noten(arr2Noten);
         assert (noten.grossteGerundete()==100);
         try {
-            noten.grossteGerundete();
+            noten1.grossteGerundete();
             assert false;
         }
         catch(Exception e)
@@ -49,9 +57,11 @@ public class TestNoten {
     public static void testDurchschnitt() throws Exception{
         int[] arrNoten = {39, 44, 57, 76, 80, 99, 17};
         Noten noten = new Noten(arrNoten);
+        int[] arr2Noten = {};
+        Noten noten1 = new Noten(arr2Noten);
         assert (noten.duchschnitt()==61);
         try {
-            noten.duchschnitt();
+            noten1.duchschnitt();
             assert false;
         }
         catch(Exception e)
